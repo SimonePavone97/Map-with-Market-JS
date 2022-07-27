d492ed5c74d36455f32e5a5ded85a6dcf52227d2
Simone Pavone <simonebest56@gmail.com>
2022-07-27 17:36:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+let mapOptions = {
+    center:[41.87194, 12.56738],
+    zoom:6
+}
+
+
+let map = new L.map('map' , mapOptions);
+
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+/*let marker = new L.Marker([45.48646,9.20597]);
+marker.addTo(map);*/
+
+let locations = [
+    {
+        "id":1,
+        "lat":45.48646,
+        "long":9.20597,
+        "src": 'img/Piazza.jpeg',
+        /*"title":,
+        "url":,*/
+    },
+    {
+        "id":2,
+        "lat":45.52998,
+        "long":9.36663,
+        "src": 'img/Togliatti.jpg',
+        /*"title":,
+        "url":,*/
+    },
+    {
+        "id":3,
+        "lat":45.48053,
+        "long":9.18604,
+        "src": 'img/Aprile.jpg',
+        /*"title":,
+        "url":,*/
+    },
+    {
+        "id":4,
+        "lat":44.64785,
+        "long":10.85484,
+        "src": 'img/entrata.jpg',
+        /*"title":,
+        "url":,*/
+    },
+    {
+        "id":5,
+        "lat":44.63461,
+        "long":10.95522,
+        "src": 'img/Emilia.jpg',
+        /*"title":,
+        "url":,*/
+    },
+    {
+        "id":6,
+        "lat":41.1076,
+        "long":16.87901,
+        "src": 'img/Orabona.jpg',
+        /*"title":,
+        "url":,*/
+    },
+    {
+        "id":7,
+        "lat":41.127,
+        "long":16.87196,
+        "src": 'img/ferrarese.jpg',
+        /*"title":,
+        "url":,*/
+    },
+]
+
+let popupOptions = {
+    "closeButton" : false
+}
+
+locations.forEach(element => {
+    new L.Marker([element.lat,element.long]).addTo(map)
+    .on("mouseover", event => {
+        event.target.bindPopup('<div class="card"><img src="'+element.src+'" width="80" height="80" alt"place"></div>', popupOptions).openPopup();
+    })
+    .on("mouseout", event => {
+        event.target.closePopup();
+    })
+});
